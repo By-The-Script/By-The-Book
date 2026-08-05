449cb753a11a6e72c7305ae3fe4c7231f8bfc3ca
Dotan <dotan.hillel@gmail.com>
2026-08-05 08:20:36 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ import { initCommunityPage } from './pages/community.js';
 import { initCommunityCreatePage } from './pages/community-create.js';
 import { initCommunityDetailsPage } from './pages/community-details.js';
 import { normalizeImagePath } from './image-paths.js';
-import { initWaitlistPage } from './Waitlist.js';
+import { initWaitlistPage } from './pages/Waitlist.js';
 
 let cachedBooks = null;
 
